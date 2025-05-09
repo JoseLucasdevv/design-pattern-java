87bf3ba6f639c56a62f03f4ebc61c72c71da8bbc
JoseLucasdevv <joselucas.dev@gmail.com>
2025-05-09 18:06:21 -0300

    lint:fix lines and add Override.

--- src/main/java/org/project/creational/factory/ConcreteTransportCar.java
+++ src/main/java/org/project/creational/factory/ConcreteTransportCar.java
@@ -2,7 +2,7 @@ package org.project.creational.factory;
 
 public class ConcreteTransportCar extends CreateTransport{
 
-
+    @Override
     public Transport createTransport(){
         return new Car();
     };
--- src/main/java/org/project/creational/factory/Transport.java
+++ src/main/java/org/project/creational/factory/Transport.java
@@ -2,5 +2,5 @@ package org.project.creational.factory;
 
 public interface Transport {
     void use();
-
+      
 }
